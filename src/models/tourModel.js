6cb6cafb31d9450700d4c4ac360738e4bfacf27d
Ramy Hakeem <ramyhakeem07@gmail.com>
2026-06-16 16:23:11 +0300

    feat: add validator package and enhance tour schema validations

--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -21,11 +21,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
-      enum: ["easy", "medium", "difficult"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium, difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
     },
     ratingsQuantity: {
       type: Number,
@@ -38,6 +43,13 @@ const tourSchema = new mongoose.Schema(
     priceDiscount: {
       type: Number,
       default: 0,
+      validate: {
+        validator: function (val) {
+          // `this` only points to the current doc on NEW document creationtions, not on updates or insertMany()
+          return val < this.price;
+        },
+        message: "Discount price {VALUE} should be below regular price",
+      },
     },
     summary: {
       type: String,
